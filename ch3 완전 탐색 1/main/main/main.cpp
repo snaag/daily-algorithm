736231d48fde15c2691596246807ca3901ba03ee
snaag <04_miffy@naver.com>
2019-09-04 15:18:16 +0900

    edit: .gitignore

---
+++ ch3 완전 탐색 1/main/main/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  main
+//
+//  Created by 이상아 on 04/09/2019.
+//  Copyright © 2019 snaag. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
